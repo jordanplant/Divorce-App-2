46f54771f5a97a4a4a954d5d7dc28e3cb6a30f7a
Jordan Plant <jordanplant94@gmail.com>
2024-01-15 11:52:07 +0000

    add form to page

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import styles from "./form.module.css";
+import "./form.css";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <div>Form</div>
+
+      <div className="form-container">
+        <div className="header">Event Reminder Form</div>
+        <form id="eventForm">
+          <input
+            type="text"
+            placeholder="Name of Event"
+            name="EventName"
+            required
+          />
+          <input type="date" placeholder="Date" name="date" required />
+          <input type="time" placeholder="Time" name="time" />
+          <input type="text" placeholder="Type of event" name="type" required />
+          <input type="text" placeholder="Event Notes" name="notes" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;

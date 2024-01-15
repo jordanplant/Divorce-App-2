5c37e7e0f82771932ba46fccd6002ca96615e101
Aidan Meehan <aodhan66@gmail.com>
2024-01-15 14:21:14 +0000

    created navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./Components/Form";
 import Months from "./Components/Months";
+import Navbar from "./Components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);

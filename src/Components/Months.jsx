db1fcf28bbbc84097480f4b831bae2c56907c2a6
Jordan Plant <jordanplant94@gmail.com>
2024-01-15 12:03:07 +0000

    Joseph-Months

---
+++ src/Components/Months.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+function Months() {
+  return (
+    <div>
+      <div>
+        <p>January</p>
+      </div>
+      <div>
+        <p>February</p>
+      </div>
+      <div>
+        <p>March</p>
+      </div>
+      <div>
+        <p>April</p>
+      </div>
+      <div>
+        <p>May</p>
+      </div>
+      <div>
+        <p>June</p>
+      </div>
+      <div>
+        <p>July</p>
+      </div>
+      <div>
+        <p>August</p>
+      </div>
+      <div>
+        <p>September</p>
+      </div>
+      <div>
+        <p>October</p>
+      </div>
+      <div>
+        <p>November</p>
+      </div>
+      <div>
+        <p>December</p>
+      </div>
+    </div>
+  );
+}
+export default Months;
